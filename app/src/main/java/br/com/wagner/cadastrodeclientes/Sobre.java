64dad84083a00cdd1f7962297fe0e5939b031d28
wagner.alves <wagner.alves.it@gmail.com>
2024-08-23 16:38:50 -0300

    Alteração para adequação as novas APIs do android

--- app/src/main/java/br/com/wagner/cadastrodeclientes/Sobre.java
+++ app/src/main/java/br/com/wagner/cadastrodeclientes/Sobre.java
@@ -1,23 +1,32 @@
-package br.com.wagner.cadastrodeclientes;
+package br.com.wagner.cadastrodeclientes; // Certifique-se de que o pacote esteja correto
 
-import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
-public class Sobre extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
 
-    Intent it;
+public class Sobre extends AppCompatActivity { // Nome da classe alterado para corresponder ao nome do arquivo
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sobre);
-    }
+        setContentView(R.layout.activity_sobre); // Certifique-se de que o layout esteja correto
 
-    public void Clientes(View v) {
+        TextView privacyPolicyTextView = findViewById(R.id.privacyPolicy);
 
-        it = new Intent(getBaseContext(), ListarActivity.class);
-        startActivity(it);
+        // Configura o link da política de privacidade
+        String privacyPolicyUrl = "https://github.com/wagneralves/CadastrodeClientesv2/blob/master/Privacy";
+        privacyPolicyTextView.setText(Html.fromHtml("<a href='" + privacyPolicyUrl + "'>Política de Privacidade</a>"));
+        privacyPolicyTextView.setMovementMethod(LinkMovementMethod.getInstance());
+
+        // Se você deseja adicionar comportamento para o clique, você pode configurar um listener, mas isso não é necessário aqui
+        privacyPolicyTextView.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(privacyPolicyUrl));
+            startActivity(intent);
+        });
     }
 }
